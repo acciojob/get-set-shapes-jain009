19d12cc4373493ffaf3945555e57a606f1f8f771
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-17 01:51:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 class Rectangle {
-  
+  constructor(width, height) {
     this._width = width;
     this._height = height;
-  
+  }
 
   get width() {
     return this._width;
@@ -18,11 +18,11 @@ class Rectangle {
 }
 
 class Square extends Rectangle {
-  constructor(side) {
+  constructor(side) {  // Fixed: Only one parameter
     super(side, side);
   }
 
   getPerimeter() {
-    return 4 * this.side;
+    return 4 * this.width;  // Use 'this.width' instead of 'this.side'
   }
 }
